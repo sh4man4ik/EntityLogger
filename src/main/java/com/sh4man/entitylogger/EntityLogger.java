e23c111ddc1c49152c825a1f7be1b11d05f11f30
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-09-12 16:53:43 +0300

    First worked version with logging in game console

--- src/main/java/com/sh4man/entitylogger/EntityLogger.java
+++ src/main/java/com/sh4man/entitylogger/EntityLogger.java
@@ -1,11 +1,17 @@
 package com.sh4man.entitylogger;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityFallingBlock;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.List;
 
 @Mod(modid = EntityLogger.MODID, name = EntityLogger.NAME, version = EntityLogger.VERSION)
 public class EntityLogger
@@ -14,20 +20,42 @@ public class EntityLogger
     public static final String NAME = "Entity Logger";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
-
-    Minecraft mc = Minecraft.getMinecraft();
+    private int tickCounter = 0;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("Hello, Minecraft");
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        Minecraft mc = Minecraft.getMinecraft();
+
+        if (event.phase != TickEvent.Phase.END || mc.world == null) {
+            return;
+        }
+
+        tickCounter++;
+        if (tickCounter != 20) {
+            return;
+        }
+        tickCounter = 0;
+
+        List<Entity> loadedEntityList = mc.world.getLoadedEntityList();
+
+        for (Entity entity : loadedEntityList) {
+            if (entity instanceof EntityFallingBlock || entity instanceof EntityPlayerSP) {
+                String name = entity.getName();
+
+                if (name.trim().isEmpty()) {
+                    continue;
+                }
+
+                System.out.printf("%s (%d) x=%.0f y=%.0f z=%.0f%n", name, entity.getEntityId(), entity.posX, entity.posY, entity.posZ);
+            }
+        }
+
+        System.out.println();
     }
 }
